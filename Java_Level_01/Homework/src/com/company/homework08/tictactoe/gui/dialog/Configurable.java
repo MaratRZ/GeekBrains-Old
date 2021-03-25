98621a537c0c0f0d936a40f08fd3f0de416b3091
Marat Zalyaletdinov <marat.rz@gmail.com>
2021-03-25 23:10:45 +0300

    Домашнее задание 8 урока

---
+++ Java_Level_01/Homework/src/com/company/homework08/tictactoe/gui/dialog/Configurable.java
@@ -0,0 +1,11 @@
+package com.company.homework08.tictactoe.gui.dialog;
+
+import com.company.homework08.tictactoe.enums.DotType;
+
+public interface Configurable {
+
+    DotType getPlayerType();
+
+    int getMapSize();
+
+}
